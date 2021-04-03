d054f363140d2fec83c3410e37c82db4d2ca963f
milklim <klimbox@gmail.com>
2021-04-03 11:43:34 +0300

    dynamic module example

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CoffeesModule } from 'src/coffees/coffees.module';
+import { DatabaseModule } from 'src/database/database.module';
 import { CoffeeRatingService } from './coffee-rating.service';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    DatabaseModule.register({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5533,
+      password: 'password',
+    }),
+    CoffeesModule,
+  ],
   providers: [CoffeeRatingService]
 })
 export class CoffeeRatingModule {}
