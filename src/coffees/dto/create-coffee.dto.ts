102f61005c2946487cc5730e223de3176981ec2c
milklim <klimbox@gmail.com>
2021-04-11 17:32:43 +0300

    update `create-coffee.dto`

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,5 +1,17 @@
+import { ArrayUnique, IsArray, IsOptional, IsString } from "class-validator";
+
 export class CreateCoffeeDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
+  @IsOptional()
+  readonly description: string;
+
+  @IsString()
   readonly brand: string;
+
+  @IsArray()
+  @ArrayUnique()
   readonly flavors: string[];
 }
