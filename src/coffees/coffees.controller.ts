f802596e35e2a2feff6c7c93e598a43c56bec785
Vladimir Klimenko <klimbox@gmail.com>
2021-04-13 21:08:48 +0300

    create custom param decorator [60]

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -10,6 +10,7 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { Protocol } from 'src/common/decorators/protocol.decorator';
 import { Public } from 'src/common/decorators/public.decorator';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { ParseIntPipe } from 'src/common/pipes/parse-int.pipe';
@@ -23,7 +24,9 @@ export class CoffeesController {
 
   @Public()
   @Get()
-  findAll(@Query() paginationQuery: PaginationQueryDto) {
+  findAll(@Protocol('tcp') protocol: string, @Query() paginationQuery: PaginationQueryDto) {
+    console.log(protocol);
+       
     return this.coffeesService.findAll(paginationQuery);
   }
   @Get(':id')
