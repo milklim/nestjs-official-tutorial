e2cc1ac94807436678e85344969bdc1519f38a71
Vladimir Klimenko <klimbox@gmail.com>
2021-04-18 20:45:45 +0300

    test(coffees.service): unit tests for `findOne` method

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,39 +1,81 @@
+import { NotFoundException } from '@nestjs/common';
 import { ConfigService, ConfigType } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesService } from './coffees.service';
 import coffeesConfig from './config/coffees.config';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+});
+
 describe('CoffeesService', () => {
   let service: CoffeesService;
+  let coffeeRepository: MockRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CoffeesService,
         { provide: Connection, useValue: {} },
-        { provide: getRepositoryToken(Flavor), useValue: {} },
-        { provide: getRepositoryToken(Coffee), useValue: {} },
+        { provide: getRepositoryToken(Flavor), useValue: createMockRepository() },
+        { provide: getRepositoryToken(Coffee), useValue: createMockRepository() },
         {
           provide: ConfigService,
           useValue: { get: (key, defaultValue) => defaultValue },
         },
         { provide: COFFEE_BRANDS, useValue: ['COFFEE_BRANDS_MOCK'] },
-        { provide: 'COFFEE_BRANDS_FACTORY', useValue: ['COFFEE_BRANDS_FACTORY_MOCK'] },
-        { provide: 'COFFEE_BRANDS_FACTORY_ASYNC', useValue: ['COFFEE_BRANDS_FACTORY_ASYNC_MOCK'] },
+        {
+          provide: 'COFFEE_BRANDS_FACTORY',
+          useValue: ['COFFEE_BRANDS_FACTORY_MOCK'],
+        },
+        {
+          provide: 'COFFEE_BRANDS_FACTORY_ASYNC',
+          useValue: ['COFFEE_BRANDS_FACTORY_ASYNC_MOCK'],
+        },
         { provide: coffeesConfig.KEY, useValue: { foo: 'mocked Foo' } },
       ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
     // service = await module.resolve(CoffeesService); // to get request scope
+    coffeeRepository = module.get<MockRepository>(getRepositoryToken(Coffee));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    describe('when coffee with ID exists', () => {
+      it('should return the coffee object', async () => {
+        const coffeeId = '1';
+        const expectedCoffee = {};
+  
+        coffeeRepository.findOne.mockReturnValue(expectedCoffee);
+        const coffee = await service.findOne(coffeeId);
+        expect(coffee).toEqual(expectedCoffee);
+      });
+    });
+  });
+  describe('otherwise', () => {
+    it('should throw the "NotFoundException"', async () => {
+      const coffeeId = '1';
+      const expectedCoffee = {};
+      coffeeRepository.findOne.mockReturnValue(expectedCoffee);
+
+      try {
+        const coffee = await service.findOne(coffeeId);
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+        expect(err.message).toEqual(`Coffee #${coffeeId} not found`);
+      }
+    });
   });
 });
