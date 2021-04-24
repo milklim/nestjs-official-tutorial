6d3f905cfb0518de3a606b22e519d2bedc3a6c31
Vladimir Klimenko <klimbox@gmail.com>
2021-04-24 18:55:01 +0300

    test: connect test DB for e2e testing

---
+++ test/coffee/coffees.e2e-spec.ts
@@ -0,0 +1,39 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoffeesModule } from '../../src/coffees/coffees.module';
+
+describe('[Feature] Coffees - /coffees', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        CoffeesModule,
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: Number(process.env.TEST_DB_PORT),
+          username: process.env.DB_USER,
+          password: process.env.DB_PASS,
+          database: process.env.DB_NAME,
+          synchronize: true,
+          autoLoadEntities: true,
+        }),
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it.todo('Create [POST /]');
+  it.todo('Get all [GET /]');
+  it.todo('Get one [GET /:id]');
+  it.todo('Update one [PATCH /:id]');
+  it.todo('Delete one [DELETE /:id]');
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
