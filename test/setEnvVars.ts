887ebd9e9c364a79ad4c3533e0445c362bb19884
Vladimir Klimenko <klimbox@gmail.com>
2021-04-24 18:50:31 +0300

    test:setup env vars for e2e tests

---
+++ test/setEnvVars.ts
@@ -0,0 +1,6 @@
+process.env.DB_HOST = 'localhost';
+process.env.TEST_DB_PORT = '5534';
+process.env.DB_USER = 'postgres';
+process.env.DB_PASS = 'pass123';
+process.env.DB_NAME = 'postgres';
+process.env.API_KEY = '1eb75569-94db-42f1-a01e-61b09a7c52b9';
